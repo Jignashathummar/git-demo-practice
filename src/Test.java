60d8376eeaa4451872f22a55d507480ce27a4a55
Jignashathummar <jignashakotadiya10@gmail.com>
2024-01-27 15:15:22 +0000

    commit2

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,6 @@
 public class Test {
     public static void main(String[] args) {
+        System.out.println("My name is Jigu,Hello");
         System.out.println("My name is Jigu, How are you?");
     }
 }
