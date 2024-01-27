f61a1fa00ad0b308912d3ac6fdc2b1bf3064382b
Jignashathummar <146828112+Jignashathummar@users.noreply.github.com>
2024-01-27 15:09:44 +0000

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("My name is Jigu");
+        System.out.println("My name is Jigu, How are you?");
     }
 }
